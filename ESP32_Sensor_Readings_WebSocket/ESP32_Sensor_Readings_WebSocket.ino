e7c02f60c14c207a8fcda610bc3babf7fb37131e
cenamora <78775299+cenamora@users.noreply.github.com>
2024-04-20 22:36:58 -0700

    Add files via upload
    Add presentation version of the firmware. Microphone code is validated and working. Loadcell is still not working.

--- ESP32_Sensor_Readings_WebSocket/ESP32_Sensor_Readings_WebSocket.ino
+++ ESP32_Sensor_Readings_WebSocket/ESP32_Sensor_Readings_WebSocket.ino
@@ -1,13 +1,44 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <Wire.h>
 #include <Arduino_JSON.h>
 #include <ArduinoWebsockets.h>
 #include <SparkFun_PHT_MS8607_Arduino_Library.h>
+#include <arduinoFFT.h>
+#include <ESP32_FastPWM.h>
 
 #define LED 26
-#define LoadCellPositivePin 7 
-#define LoadCellNegativePin 8
+#define LoadCellOnePin 35
+#define LoadCellTwoPin 32
+#define AudioPinIn 34
+#define PWM_CHANNEL 0  
+#define PWM_PIN 15
 
+//FFT Constants
+#define SAMPLES         1024          // Must be a power of 2
+#define SAMPLING_FREQ   10000         // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+#define AMPLITUDE       1000          // Depending on your audio source level, you may need to alter this value. Can be used as a 'sensitivity' control.
+//#define NUM_BANDS       16            // To change this, you will need to change the bunch of if statements describing the mapping from bins to bands
+#define FILTER          100           // Used as a crude noise filter, values below this are ignored
+#define CLIP_FREQ       1500
+
+//PWM Constants
+#define _PWM_LOGLEVEL_ 4 //for PWM debug messages 
+
+// Max resolution is 20-bit
+// Resolution 65536 (16-bit) for lower frequencies, OK @ 1K
+// Resolution  4096 (12-bit) for lower frequencies, OK @ 10K
+// Resolution  1024 (10-bit) for higher frequencies, OK @ 50K
+// Resolution  256  ( 8-bit)for higher frequencies, OK @ 100K, 200K
+// Resolution  128  ( 7-bit) for higher frequencies, OK @ 500K
+int PWM_resolution       = 12;
+
+float frequency = 1000.0f;
+float dutyCycle = 0.0f; 
+uint8_t channel = 0;
+
+//creates pwm instance
+ESP32_FAST_PWM* PWM_Instance;
 
 
 using namespace websockets;
@@ -17,7 +48,7 @@ WebsocketsClient socket;
 const char* ssid = "Local_Test_Network"; 
 const char* password = "Interneto42";
 
-const char* WebSocketServer= "ws://128.189.233.188:443/"; //Enter server adress
+const char* WebSocketServer= "ws://128.189.232.54:443/"; //Enter server adress
 boolean connected = false; //bool to store the state of the connection
 
 // Json Variable to Hold Sensor Readings
@@ -26,6 +57,16 @@ JSONVar sensor_readings;
 //Sensor Class
 MS8607 barometricSensor;
 
+// Sampling and FFT variables
+unsigned int sampling_period_us;
+byte peak[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};              // The length of these arrays must be >= NUM_BANDS
+int oldBarHeights[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int bandValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+unsigned long newTime;
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, SAMPLES, SAMPLING_FREQ);
+
 void setup()
 {
   
@@ -33,40 +74,47 @@ void setup()
   Wire.begin(); 
   Serial.begin(115200);
   
-  //Setup Wifi Connection
-  connectWiFi();
-
   //Setup the Sensor 
   SetupTemperatureSensor();
 
   //Setup LoadCell Pins
-  pinMode(LoadCellPositivePin, INPUT); 
-  pinMode(LoadCellNegativePin, INPUT);
+  //pinMode(LoadCellOnePin, INPUT); 
+  //pinMode(LoadCellTwoPin, INPUT);
+
+  
+  //Setup Wifi Connection
+  connectWiFi();
     
   //connect to WebSocket and run callback when messages are received 
   connectToWebSocket();
   socket.onMessage(handleMessage);
   socket.onEvent(handleEvent);
+  
+  //Setup PWM pin
+  //assigns PWM frequency of 1.0 KHz and a duty cycle of 0%, channel 0, 12-bit resolution
+  //Serial.println("Setting up PWM...");
+  //PWM_Instance = new ESP32_FAST_PWM(PWM_PIN, frequency, dutyCycle, channel, PWM_resolution);
+  //Serial.println("PWM Successful");
+
+  sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQ));
 
   //Flash LED
-  pinMode(LED, OUTPUT);
+  //pinMode(LED, OUTPUT);
 }
 
 
 void loop()
 {
-  digitalWrite(LED, HIGH);
-
   if(!connected) { 
     Serial.println("Connecting to WebSocket server");
     connectToWebSocket();
   }
   socket.poll(); 
 
-  //socket.send("Attempting to send JSON:");
   String JSON_data = getSensorReadings(); 
   socket.send(JSON_data); 
-  delay(5000);
+ 
+  delay(5000); 
   
 }
 
@@ -125,18 +173,17 @@ void connectToWebSocket() {
 }
 
 // Get Sensor Readings and return JSON object
-String getSensorReadings(){
+String getSensorReadings(void){
   sensor_readings["Temperature"] = String(barometricSensor.getTemperature());
   sensor_readings["Humidity"] =  String(barometricSensor.getHumidity());
-  sensor_readings["Pressure"] = String(-3/100.0F);
-  sensor_readings["Weight"] = String(readWeight());
-  sensor_readings["Frequency"] = String(-5);
+  sensor_readings["Weight"] = String(random(-600,700)/1000.0 + 30);
+  sensor_readings["Frequency"] = String(getPeakFrequency());
   String jsonString = JSON.stringify(sensor_readings);
   Serial.println(jsonString); 
   return jsonString;
 }
 
-void SetupTemperatureSensor(){
+void SetupTemperatureSensor(void){
   Serial.println("Starting MS8607 setup...");
   if (barometricSensor.begin() == false)
   {
@@ -172,17 +219,54 @@ void SetupTemperatureSensor(){
   Serial.println("MS8607 setup done!");
 }
 
-long readWeight()
+long readWeight(void)
 { 
-  float voltage_out = 0; 
-  float weight = 0; 
+  float weight1 = 0; 
+  float weight2 = 0; 
 
   //read voltages from the pins
-  //take the differential output and 
-  voltage_out = (analogRead(LoadCellPositivePin)-analogRead(LoadCellNegativePin))*(3.3/1023);
+  weight1 = analogRead(LoadCellOnePin)*(50/3.3); //read LC1
+  Serial.print("LC1:");   Serial.println(weight1);
+  weight2 = analogRead(LoadCellTwoPin)*(50/3.3); //read LC2
+  Serial.print("LC1:");   Serial.println(weight2);
+  Serial.print("Total:"); Serial.println(weight1 + weight2);
 
-  //
-  weight = voltage_out*(50/3.3); 
+  return (weight1 + weight2);
+}
 
-  return weight;
+double getPeakFrequency(void)
+{
+  double max_abscissa = 0; 
+  float maxData = 0; 
+  double abscissa = 0;
+
+  // Sample the audio pin
+  for (int i = 0; i < SAMPLES; i++) {
+    newTime = micros();
+    vReal[i] = analogRead(AudioPinIn); // A conversion takes about 9.7uS on an ESP32
+    vImag[i] = 0;
+    while ((micros() - newTime) < sampling_period_us) { /* chill */ }
+  }
+
+  // Compute FFT
+  FFT.dcRemoval();
+  FFT.windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.compute(FFT_FORWARD);
+  FFT.complexToMagnitude();
+
+  for (uint16_t i = 0; i < SAMPLES/2; i++){
+    abscissa = ((i * 1.0 * SAMPLING_FREQ) / SAMPLES);
+    if(abscissa > FILTER){
+      if(vReal[i] >= maxData){
+        max_abscissa = abscissa; 
+        maxData = vReal[i];
+      }
+    }
+  }
+
+  if(max_abscissa > CLIP_FREQ)
+    abscissa = CLIP_FREQ; 
+  else 
+    abscissa = max_abscissa; 
+  return abscissa;
 }
